59b9fbd49ea5cf2064177a80e061258bf5a92ff4
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-05-18 22:10:56 +0530

    Sarthak | Refactors read_buffer benchmark

--- benches/benchmarks/read_buffer.rs
+++ benches/benchmarks/read_buffer.rs
@@ -1,19 +1,18 @@
-use std::ops::Div;
 use std::sync::Arc;
-use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 use criterion::{Criterion, criterion_group, criterion_main};
 
-use cached::cache::buffer_event::BufferConsumer;
 use cached::cache::proxy::admission_policy::ProxyAdmissionPolicy;
 use cached::cache::proxy::pool::ProxyPool;
-use cached::cache::types::{TotalCapacity, TotalCounters, TotalShards, Weight};
+use cached::cache::types::{KeyHash, TotalCapacity, TotalCounters, TotalShards, Weight};
+
+use crate::benchmarks::common::execute_parallel;
 
 const COUNTERS: TotalCounters = 2 << 20;
 const CAPACITY: TotalCapacity = 1_000_000;
 const SHARDS: TotalShards = 256;
-const CACHE_WEIGHT: Weight = 10_000_000;
+const CACHE_WEIGHT: Weight = CAPACITY as Weight;
 
 #[cfg(feature = "bench_testable")]
 pub fn read_buffer_one_thread(criterion: &mut Criterion) {
@@ -31,59 +30,26 @@ pub fn read_buffer_one_thread(criterion: &mut Criterion) {
 
 #[cfg(feature = "bench_testable")]
 pub fn read_buffer_8_threads(criterion: &mut Criterion) {
-    let consumer = Arc::new(ProxyAdmissionPolicy::<u64>::new(COUNTERS, CAPACITY, SHARDS, CACHE_WEIGHT));
-    let pool = Arc::new(ProxyPool::new(32, 64, consumer));
-    read_buffer_parallel(criterion, "Pool.add() | 8 threads", pool, 8);
+    execute_parallel(criterion, "Pool.add() | 8 threads", prepare_execution_block(), 8);
 }
 
 #[cfg(feature = "bench_testable")]
 pub fn read_buffer_16_threads(criterion: &mut Criterion) {
-    let consumer = Arc::new(ProxyAdmissionPolicy::<u64>::new(COUNTERS, CAPACITY, SHARDS, CACHE_WEIGHT));
-    let pool = Arc::new(ProxyPool::new(32, 64, consumer));
-    read_buffer_parallel(criterion, "Pool.add() | 16 threads", pool, 16);
+    execute_parallel(criterion, "Pool.add() | 16 threads", prepare_execution_block(), 16);
 }
 
 #[cfg(feature = "bench_testable")]
 pub fn read_buffer_32_threads(criterion: &mut Criterion) {
-    let consumer = Arc::new(ProxyAdmissionPolicy::<u64>::new(COUNTERS, CAPACITY, SHARDS, CACHE_WEIGHT));
-    let pool = Arc::new(ProxyPool::new(32, 64, consumer));
-    read_buffer_parallel(criterion, "Pool.add() | 32 threads", pool, 32);
+    execute_parallel(criterion, "Pool.add() | 32 threads", prepare_execution_block(), 32);
 }
 
-#[cfg(feature = "bench_testable")]
-pub fn read_buffer_parallel<'a, Consumer: BufferConsumer + Send + Sync + 'a + 'static>(
-    criterion: &'a mut Criterion,
-    id: &'static str,
-    pool: Arc<ProxyPool<Consumer>>,
-    thread_count: u8) {
-    criterion.bench_function(id, |bencher| bencher.iter_custom(|iterations| {
-        let per_thread_iterations = iterations / thread_count as u64;
-        let mut current_start = 0;
-        let mut current_end = current_start + per_thread_iterations;
+fn prepare_execution_block() -> Arc<impl Fn(u64) -> () + Send + Sync + 'static> {
+    let consumer = Arc::new(ProxyAdmissionPolicy::<u64>::new(COUNTERS, CAPACITY, SHARDS, CACHE_WEIGHT));
+    let pool = Arc::new(ProxyPool::new(32, 64, consumer));
 
-        let mut threads = Vec::new();
-        for thread_id in 1..=thread_count {
-            threads.push(thread::spawn({
-                let pool = pool.clone();
-                move || {
-                    let start = Instant::now();
-                    for _index in current_start..current_end {
-                        pool.add(thread_id as u64);
-                    }
-                    start.elapsed()
-                }
-            }));
-            current_start = current_end;
-            current_end += per_thread_iterations;
-        }
-
-        let mut total_time = Duration::from_nanos(0);
-        for thread in threads {
-            let elapsed = thread.join().unwrap();
-            total_time += elapsed;
-        }
-        total_time.div(thread_count as u32)
-    }));
+    Arc::new(move |index| {
+        pool.add(index);
+    })
 }
 
 criterion_group!(benches, read_buffer_one_thread, read_buffer_8_threads, read_buffer_16_threads, read_buffer_32_threads);
