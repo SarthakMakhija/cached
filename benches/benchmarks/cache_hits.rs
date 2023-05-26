737f5b398ed8b6b95a0152860fa4429f7aafe749
Sarthak Makhija <sarthak.makhija@gmail.com>
2023-05-26 15:21:58 +0530

    Sarthak | Removes some comments from cache_hits

--- benches/benchmarks/cache_hits.rs
+++ benches/benchmarks/cache_hits.rs
@@ -17,10 +17,6 @@ const CAPACITY: usize = 100_000;
 const COUNTERS: TotalCounters = (CAPACITY * 10) as TotalCounters;
 
 /// Defines the total size of the cache.
-/// It is kept to CAPACITY * 40 because the benchmark inserts keys and values of type u64.
-/// Weight of a single u64 key and u64 value without time_to_live is 40 bytes. Check `src/cache/config/weight_calculation.rs`
-/// As a part of this benchmark, we preload the cache with the total number of elements = CAPACITY.
-/// We want all the elements to be admitted in the cache, hence weight = CAPACITY * 40 bytes.
 const WEIGHT: Weight = (CAPACITY * 40) as Weight;
 
 /// Defines the total sample size that is used for generating Zipf distribution.
